e0e0f7354199976875e819603ccfa8770fd63d5e
Alexandru Loghin <loghin.dev@gmail.com>
2025-05-19 19:05:53 +0300

    added spinner functionality, changed font

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const gameboard = document.querySelector(".gameboard");
+const p1Spinner = document.querySelector(".scoreboard .player-1 .spinner");
+const p2Spinner = document.querySelector(".scoreboard .player-2 .spinner");
 
 const THREE = 3;
+const player1 = ["X", "O"][Math.floor(Math.random() * 2)];
+const player2 = player1 === "X" ? "0" : "X";
+let current = [player1, player2][Math.floor(Math.random() * 2)];
 
 document.addEventListener("DOMContentLoaded", () => {
   generateGameboard();
+  whichTurn(current);
 });
 
 function generateGameboard() {
@@ -18,5 +24,21 @@ function generateGameboard() {
 }
 
 function setCell(event) {
-  console.log(event.target);
+  event.target.textContent = current;
+  updateCurrent();
+}
+
+function updateCurrent() {
+  current === player1 ? (current = player2) : (current = player1);
+  whichTurn(current);
+}
+
+function whichTurn(subject) {
+  if (subject === player1) {
+    p1Spinner.classList.replace("d-none", "thinking");
+    p2Spinner.classList.replace("thinking", "d-none");
+  } else {
+    p2Spinner.classList.replace("d-none", "thinking");
+    p1Spinner.classList.replace("thinking", "d-none");
+  }
 }
