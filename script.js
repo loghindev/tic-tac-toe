6c799c5384670ed74d59577182a3a46f793e2478
Alexandru Loghin <loghin.dev@gmail.com>
2025-05-20 19:28:56 +0300

    fixed minor bug

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const gameboard = document.querySelector(".gameboard");
+const p1Name = document.querySelector(".scoreboard .player-1 .name");
+const p2Name = document.querySelector(".scoreboard .player-2 .name");
 const p1Spinner = document.querySelector(".scoreboard .player-1 .spinner");
 const p2Spinner = document.querySelector(".scoreboard .player-2 .spinner");
+const p1Score = document.querySelector(".scoreboard .player-1 .value");
+const p2Score = document.querySelector(".scoreboard .player-2 .value");
+const tieScore = document.querySelector(".scoreboard .tie .value");
 
 const THREE = 3;
 const player1 = ["X", "O"][Math.floor(Math.random() * 2)];
 const player2 = player1 === "X" ? "0" : "X";
 let current = [player1, player2][Math.floor(Math.random() * 2)];
 
+let gameOver = false;
+
 document.addEventListener("DOMContentLoaded", () => {
   generateGameboard();
+  loadUsernames();
   whichTurn(current);
 });
 
@@ -24,8 +32,14 @@ function generateGameboard() {
 }
 
 function setCell(event) {
+  if (event.target.textContent !== "" || gameOver) return;
   event.target.textContent = current;
   updateCurrent();
+}
+
+function loadUsernames() {
+  p1Name.textContent = `Player 1 (${player1})`;
+  p2Name.textContent = `Player 2 (${player2})`;
 }
 
 function updateCurrent() {
@@ -37,7 +51,7 @@ function whichTurn(subject) {
   if (subject === player1) {
     p1Spinner.classList.replace("d-none", "thinking");
     p2Spinner.classList.replace("thinking", "d-none");
-  } else {
+  } else if (subject === player2) {
     p2Spinner.classList.replace("d-none", "thinking");
     p1Spinner.classList.replace("thinking", "d-none");
   }
